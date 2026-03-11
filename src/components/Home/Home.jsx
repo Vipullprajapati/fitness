162da4077630804b46e9eabcddc9329839fec581
Vipul Prajapati <vipulprajapati0070@gmail.com>
2026-03-11 08:29:42 +0000

    update in about

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import gymheadingimg from "../../assets/gym Heading img.jpeg";
 
-import strengthzone from "../../assets/strength zone.jpeg"; 
+import strengthzone from "../../assets/strength zone.jpeg";
 import functionaltraining from "../../assets/functional traning.jpeg";
 import personaltraining from "../../assets/personal traning.jpeg";
 import cardiodeck from "../../assets/cardio deck.jpeg";
@@ -28,14 +28,16 @@ export default function Home() {
         <div className="relative max-w-7xl mx-auto px-4 py-24 sm:px-6 lg:px-8">
           <div className="max-w-xl sm:mt-1 space-y-8">
             <h1 className="text-5xl font-extrabold tracking-tight sm:text-6xl">
-              <span className="text-orange-500">Fitsensefitnesshub</span>
+              <span className="text-orange-500">Vipul Prajapati</span>
             </h1>
             <p className="text-xl text-gray-300">
-              High-performance training facility with cutting-edge equipment and
-              expert coaching to transform your body and mind.
+              I am Vipul Prajapati I have 7 years of progressive
+              experience in end-to-end store operations, staff management, and
+              overseeing all ground-level retail activities. Worked with reputed
+              industry leaders, including Reliance, Blinkit, and Flipkart
             </p>
             <div className="flex flex-wrap gap-4">
-              <Link 
+              <Link
                 to="/contact"
                 className="inline-flex items-center px-6 py-3 font-medium bg-orange-600 rounded-lg hover:bg-orange-700 transition-colors"
               >
@@ -57,11 +59,8 @@ export default function Home() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
             <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-              Why Choose Fitsensefitnesshub
+              Why Choose E-Commerces
             </h2>
-            <p className="mt-4 max-w-2xl text-xl text-gray-600 mx-auto">
-              We're more than just a gym - we're a fitness community
-            </p>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
